8cbcaa2f76edbebaeb88fcd359ca2c83aa0033f8
YogeshAdhithya0609 <95347828+YogeshAdhithya0609@users.noreply.github.com>
2022-02-24 11:29:25 +0530

    Add files via upload

---
+++ QUIZ_APP/q.js
@@ -0,0 +1,237 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The correct sequence of HTML tags for starting a webpage is",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML, Head, Title, Body"
+    ]
+  },
+   {
+    numb: 7,
+    question: "The hr tag in HTML is used for",
+    answer: "horizontal ruler",
+    options: [
+      "horizontal ruler",
+      "new line",
+      "vertical ruler",
+      "new paragraph"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What are the types of unordered or bulleted list in HTML?",
+    answer: "disc, circle, square",
+    options: [
+      "disc, square, triangle",
+      "polygon, triangle, circle",
+      "disc, circle, square",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 9,
+    question: " Which of the following HTML attribute is used to define inline styles?",
+    answer: " style",
+    options: [
+      "style",
+      "type",
+      "class",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: " An HTML program is saved by using the ____ extension.",
+    answer: ".html",
+    options: [
+      ".hml",
+      ".csss",
+      ".html",
+      ".ht"
+    ]
+  },
+  {
+    numb: 11,
+    question: "A program in HTML can be rendered and read by ",
+    answer: "Web browser",
+    options: [
+      "Server",
+      "Interpreter",
+      "Web browser",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 12,
+    question: "The tags in HTML are",
+    answer: "not case sensitive",
+    options: [
+      "case-sensitive",
+      "in upper case",
+      "not case sensitive",
+      "in lowercase"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which of the following are the attributes of the tag?",
+    answer: "Both (a) & (b)",
+    options: [
+      "method",
+      "action",
+      "Both (a) & (b)",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which attribute specifies a unique alphanumeric identifier to be associated with an element?",
+    answer: "id",
+    options: [
+      "class",
+      "id",
+      "article",
+      "html"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which attribute is used to provide an advisory text about an element or its contents?",
+    answer: "title",
+    options: [
+      "tooltip",
+      "dir",
+      "head",
+      "title"
+    ]
+  },
+  {
+    numb: 16,
+    question: "The __________ attribute sets the text direction as related to the lang attribute.",
+    answer: "dir",
+    options: [
+      "dir",
+      "lang",
+      "sub",
+      "ds"
+    ]
+  },
+  {
+    numb: 17,
+    question: " Which of the following is the attribute that specifies the column name from the data source object that supplies the bound data?",
+    answer: "datafld",
+    options: [
+      "disabled",
+      "datafld",
+      "dataFormatAs",
+      "datasrc"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which of the following is the attribute that is used to set a global identifier for a microdata item?",
+    answer: "itemid",
+    options: [
+      "itemid",
+      "key",
+      "id",
+      " itemclass"
+    ]
+  },
+  {
+    numb: 19,
+    question: " HTML is a subset of",
+    answer: "SGML",
+    options: [
+      "SGMD",
+      "SGML",
+      "SGMH",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which of the following is the attribute that is used to add a name/value pair to a microdata item?",
+    answer: "itemprop",
+    options: [
+      "itemscope",
+      "itemprop",
+      "itemref",
+      "itemid"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 8,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
